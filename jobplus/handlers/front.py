2873365bcd038094b3fe5c5a7cf4d5e0ac0b6491
myday56 <leletou56@live.com>
2018-10-16 00:30:49 +0800

    fix index/job.html time

--- jobplus/handlers/front.py
+++ jobplus/handlers/front.py
@@ -1,6 +1,7 @@
 from jobplus.models import Job,Company
 from flask import render_template,Blueprint,flash,request,current_app
 from flask_login import login_user,logout_user,login_required
+from datetime import datetime
 
 front = Blueprint('front',__name__)
 
@@ -9,8 +10,9 @@ def index():
 
     job=Job.query.all()
     company = Company.query.all()
+    time_now=datetime.utcnow()
     
-    return render_template('index.html',job=job,company=company)
+    return render_template('index.html',job=job,company=company,time=time_now)
 
 @front.route('/userregister')
 def userregister():
@@ -40,11 +42,13 @@ def logout():
 @front.route('/job')
 def job():
     job=Job.query.all()
+    time_now=datetime.utcnow()
+
     page = request.args.get('page',default=1,type=int)
     pagination = Job.query.paginate(
         page = page,
         per_page = current_app.config['INDEX_PER_PAGE'],
         error_out = False
     )
-    return render_template('./job/job.html',pagination=pagination,job=job)
+    return render_template('./job/job.html',pagination=pagination,job=job,time=time_now)
 
