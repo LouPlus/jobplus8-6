e3f9a96c903be3620d739cd584ad39cc2f78ec0a
ccbmarcowang <490795201@qq.com>
2018-10-22 23:42:38 +0800

    generator and models

--- jobplus/generator.py
+++ jobplus/generator.py
@@ -4,21 +4,22 @@ from jobplus.models import db,Job,User,CompanyDetail
 fake = Factory().create('zh_CN')
 
 def iter_job():
-    for i in range(50):
-        yield Job(
-        jobname=fake.job(),
-        salary_min = random.randint(2,5) * 1000,
-        salary_max = random.randint(6,10) * 1000,
-        exprience = random.randint(0,5),
-        location = fake.city(),
-        job_tag = fake.word(),
-        job_description = fake.word(),
-        job_requirement = fake.sentence(),
-        company_id = random.randint(1,15)
-        )
+    for company in CompanyDetail.query:
+        for i in range(2):
+            yield Job(
+            jobname=fake.job(),
+            salary_min = random.randint(2,5) * 1000,
+            salary_max = random.randint(6,10) * 1000,
+            exprience = random.randint(0,5),
+            location = fake.city(),
+            job_tag = fake.word(),
+            job_description = fake.word(),
+            job_requirement = fake.sentence(),
+            company_id = company.id
+            )
 
 def iter_user():
-    for i in range(15):
+    for i in range(10):
         yield User(
         username = fake.name(),
         email = fake.ascii_safe_email(),
@@ -26,12 +27,23 @@ def iter_user():
         _password = fake.password(),
         phone=fake.phone_number(),
         realname=fake.name(),
-        role=random.choice([10,20])
+        role=10
+        )
+
+def iter_companyuser():
+    for i in range(10):
+        yield User(
+        username = fake.company(),
+        email = fake.ascii_safe_email(),
+        job = fake.job(),
+        _password = fake.password(),
+        phone=fake.phone_number(),
+        realname=fake.name(),
+        role=20
         )
 
 def iter_company():
-
-    for i in range(10):
+    for user in User.query.filter_by(role=20):
         yield CompanyDetail(
         logo = fake.url(),
         site = fake.url(),
@@ -44,17 +56,21 @@ def iter_company():
         welfares = fake.word(),
         field = fake.word(),
         finance_stage = fake.word(),
+        user_id=user.id
         )
 
 def run():
     for user in iter_user():
         db.session.add(user)
 
-    for job in iter_job():
-        db.session.add(job)
+    for companyuser in iter_companyuser():
+        db.session.add(companyuser)
 
     for company in iter_company():
         db.session.add(company)
+
+    for job in iter_job():
+        db.session.add(job)
 
     try:
         db.session.commit()
